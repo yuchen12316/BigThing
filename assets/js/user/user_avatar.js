e542d2dd34c47423c66e4df8bd8ecc93fb4c29ab
Yuchen <Yuchen@ziwei.com>
2020-12-07 20:48:54 +0800

    修改了密码头像和信息页面

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   var layer = layui.layer
 
   // 1.1 获取裁剪区域的 DOM 元素
@@ -14,13 +14,12 @@ $(function() {
   // 1.3 创建裁剪区域
   $image.cropper(options)
 
-  // 为上传按钮绑定点击事件
-  $('#btnChooseImage').on('click', function() {
+  $('#btnChooseImage').on('click', function () {
     $('#file').click()
   })
 
   // 为文件选择框绑定 change 事件
-  $('#file').on('change', function(e) {
+  $('#file').on('change', function (e) {
     // 获取用户选择的文件
     var filelist = e.target.files
     if (filelist.length === 0) {
@@ -29,10 +28,8 @@ $(function() {
 
     // 1. 拿到用户选择的文件
     var file = e.target.files[0]
-    // console.log(file)
     // 2. 将文件，转化为路径
     var imgURL = URL.createObjectURL(file)
-    // console.log(imgURL)
     // 3. 重新初始化裁剪区域
     $image
       .cropper('destroy') // 销毁旧的裁剪区域
@@ -40,24 +37,22 @@ $(function() {
       .cropper(options) // 重新初始化裁剪区域
   })
 
-  // 为确定按钮，绑定点击事件
-  $('#btnUpload').on('click', function() {
-    // 1. 要拿到用户裁剪之后的头像
+  $('#btnUpload').on('click', function () {
     var dataURL = $image
       .cropper('getCroppedCanvas', {
         // 创建一个 Canvas 画布
         width: 100,
         height: 100
       })
-      .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
-    // 2. 调用接口，把头像上传到服务器
+      .toDataURL('image/png')
+
     $.ajax({
       method: 'POST',
       url: '/my/update/avatar',
       data: {
         avatar: dataURL
       },
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('更换头像失败！')
         }
